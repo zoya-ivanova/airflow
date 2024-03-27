cc8bb894d727bfe5348edcda8fb98a94653be09c
zoya-ivanova <116255881+zoya-ivanova@users.noreply.github.com>
2024-03-27 11:46:58 +0300

    Update sem7_1.py

--- dags/sem7_1.py
+++ dags/sem7_1.py
@@ -8,37 +8,37 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.email_operator import EmailOperator
 
 @dag(
-dag_id="process-employees-v3",
-schedule_interval="0 0 * * *",
-start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
-catchup=False,
-dagrun_timeout=datetime.timedelta(minutes=60),
+    dag_id="process-employees-v3",
+    schedule_interval="0 0 * * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
 )
 def ProcessEmployees():
     create_employees_table = PostgresOperator(
-    task_id="create_employees_table",
-    postgres_conn_id="postgress_conn",
-    sql="""
-    CREATE TABLE IF NOT EXISTS employees (
-    "Serial Number" NUMERIC PRIMARY KEY,
-    "Company Name" TEXT,
-    "Employee Markme" TEXT,
-    "Description" TEXT,
-    "Leave" INTEGER
-    );""",
+        task_id="create_employees_table",
+        postgres_conn_id="postgress_conn",
+        sql="""
+            CREATE TABLE IF NOT EXISTS employees (
+            "Serial Number" NUMERIC PRIMARY KEY,
+            "Company Name" TEXT,
+            "Employee Markme" TEXT,
+            "Description" TEXT,
+            "Leave" INTEGER
+            );""",
     )   
     create_employees_temp_table = PostgresOperator(
-    task_id="create_employees_temp_table",
-    postgres_conn_id="postgress_conn",
-    sql="""
-    DROP TABLE IF EXISTS employees_temp;
-    CREATE TABLE employees_temp (
-    "Serial Number" NUMERIC PRIMARY KEY,
-    "Company Name" TEXT,
-    "Employee Markme" TEXT,
-    "Description" TEXT,
-    "Leave" INTEGER
-    );""",
+        task_id="create_employees_temp_table",
+        postgres_conn_id="postgress_conn",
+        sql="""
+            DROP TABLE IF EXISTS employees_temp;
+            CREATE TABLE employees_temp (
+            "Serial Number" NUMERIC PRIMARY KEY,
+            "Company Name" TEXT,
+            "Employee Markme" TEXT,
+            "Description" TEXT,
+            "Leave" INTEGER
+            );""",
     )
 
     @task
@@ -50,31 +50,31 @@ def ProcessEmployees():
         response = requests.request("GET", url)
         with open(data_path+"/data_file.csv", "w") as file:
             file.write(response.text)
-            postgres_hook = PostgresHook(postgres_conn_id="postgress_conn")
-            conn = postgres_hook.get_conn()
-            cur = conn.cursor()
+        postgres_hook = PostgresHook(postgres_conn_id="postgress_conn")
+        conn = postgres_hook.get_conn()
+        cur = conn.cursor()
         with open(data_path+"/data_file.csv", "r") as file:
             cur.copy_expert(
-            "COPY employees_temp FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
-            file,
+                "COPY employees_temp FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
+                file,
             )
-            conn.commit()
+        conn.commit()
 
     @task
     def merge_data(ti=None):
         count_q = """
-        SELECT COUNT(*)
-        FROM employees
+            SELECT COUNT(*)
+            FROM employees
         """
         query = """
-        INSERT INTO employees
-        SELECT *
-        FROM (
-        SELECT DISTINCT *
-        FROM employees_temp
-        )
-        ON CONFLICT ("Serial Number") DO UPDATE
-        SET "Serial Number" = excluded."Serial Number";
+            INSERT INTO employees
+            SELECT *
+            FROM (
+                SELECT DISTINCT *
+                FROM employees_temp
+            )
+            ON CONFLICT ("Serial Number") DO UPDATE
+            SET "Serial Number" = excluded."Serial Number";
         """
         try:
             postgres_hook = PostgresHook(postgres_conn_id="postgress_conn")
