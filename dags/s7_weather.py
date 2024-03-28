5ba227159a0699d6734809ad311587ae36c660ac
zoya-ivanova <116255881+zoya-ivanova@users.noreply.github.com>
2024-03-28 11:17:38 +0300

    Update s7_weather.py

--- dags/s7_weather.py
+++ dags/s7_weather.py
@@ -7,7 +7,7 @@ import requests
 
 
 OPENWEATHER_KEY = Variable.get("secret_openweather_key") 
-URL = f'http://api.openweathermap.org/geo/1.0/direct?q=Cairo&limit=5&appid={OPENWEATHER_KEY}' 
+# URL = f'http://api.openweathermap.org/geo/1.0/direct?q=Cairo&limit=5&appid={OPENWEATHER_KEY}' 
 URL_OPENWEATHER = f'/data/2.5/weather?lat=59.57&lon=30.19&exclude=current&appid={OPENWEATHER_KEY}&units=metric'
 
 def choosing_description_weather(ti):
